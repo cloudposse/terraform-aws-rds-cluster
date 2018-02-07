11980551c26e1227f6ca7058afad67227a81ffdd
aknysh <aknysh>
2018-02-07 15:37:48 -0500

    terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "name" {
-  value = "${aws_rds_cluster.default.database_name}"
+  value       = "${aws_rds_cluster.default.database_name}"
   description = "Database name"
 }
 
 output "user" {
-  value = "${aws_rds_cluster.default.master_username}"
+  value       = "${aws_rds_cluster.default.master_username}"
   description = "Username for the master DB user"
 }
 
 output "password" {
-  value = "${aws_rds_cluster.default.master_password}"
+  value       = "${aws_rds_cluster.default.master_password}"
   description = "Password for the master DB user"
 }
 
 output "master_host" {
-  value = "${module.dns_master.hostname}"
+  value       = "${module.dns_master.hostname}"
   description = "DB Master hostname"
 }
 
 output "replicas_host" {
-  value = "${module.dns_replicas.hostname}"
+  value       = "${module.dns_replicas.hostname}"
   description = "Replicas hostname"
 }
 
 output "cluster_name" {
-  value = "${aws_rds_cluster.default.cluster_identifier}"
+  value       = "${aws_rds_cluster.default.cluster_identifier}"
   description = "Cluster Identifier"
 }
