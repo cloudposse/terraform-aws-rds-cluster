1b24ab5bd9e5fce3c86cd9231c4452d5fb296081
rverma-nikiai <rohit.verma@niki.ai>
2019-06-21 16:38:34 +0530

    upgraded-tf

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,50 @@
 output "name" {
-  value       = "${join("", aws_rds_cluster.default.*.database_name)}"
+  value       = join("", aws_rds_cluster.default.*.database_name)
   description = "Database name"
 }
 
 output "user" {
-  value       = "${join("", aws_rds_cluster.default.*.master_username)}"
+  value       = join("", aws_rds_cluster.default.*.master_username)
   description = "Username for the master DB user"
 }
 
 output "cluster_name" {
-  value       = "${join("", aws_rds_cluster.default.*.cluster_identifier)}"
+  value       = join("", aws_rds_cluster.default.*.cluster_identifier)
   description = "Cluster Identifier"
 }
 
 output "arn" {
-  value       = "${join("", aws_rds_cluster.default.*.arn)}"
+  value       = join("", aws_rds_cluster.default.*.arn)
   description = "Amazon Resource Name (ARN) of cluster"
 }
 
 output "endpoint" {
-  value       = "${join("", aws_rds_cluster.default.*.endpoint)}"
+  value       = join("", aws_rds_cluster.default.*.endpoint)
   description = "The DNS address of the RDS instance"
 }
 
 output "reader_endpoint" {
-  value       = "${join("", aws_rds_cluster.default.*.reader_endpoint)}"
+  value       = join("", aws_rds_cluster.default.*.reader_endpoint)
   description = "A read-only endpoint for the Aurora cluster, automatically load-balanced across replicas"
 }
 
 output "master_host" {
-  value       = "${module.dns_master.hostname}"
+  value       = module.dns_master.hostname
   description = "DB Master hostname"
 }
 
 output "replicas_host" {
-  value       = "${module.dns_replicas.hostname}"
+  value       = module.dns_replicas.hostname
   description = "Replicas hostname"
 }
 
 output "dbi_resource_ids" {
-  value       = ["${aws_rds_cluster_instance.default.*.dbi_resource_id}"]
+  value       = [aws_rds_cluster_instance.default.*.dbi_resource_id]
   description = "List of the region-unique, immutable identifiers for the DB instances in the cluster."
 }
 
 output "cluster_resource_id" {
-  value       = "${join("", aws_rds_cluster.default.*.cluster_resource_id )}"
+  value       = join("", aws_rds_cluster.default.*.cluster_resource_id)
   description = "The region-unique, immutable identifie of the cluster."
 }
+
--- variables.tf
+++ variables.tf
@@ -1,161 +1,161 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `eg` or `cp`)"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Name of the application"
 }
 
 variable "zone_id" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Route53 parent zone ID. If provided (not empty), the module will create sub-domain DNS records for the DB master and replicas"
 }
 
 variable "security_groups" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of security groups to be allowed to connect to the DB instance"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "VPC ID to create the cluster in (e.g. `vpc-a22222ee`)"
 }
 
 variable "subnets" {
-  type        = "list"
+  type        = list(string)
   description = "List of VPC subnet IDs"
 }
 
 variable "instance_type" {
-  type        = "string"
+  type        = string
   default     = "db.t2.small"
   description = "Instance type to use"
 }
 
 variable "cluster_size" {
-  type        = "string"
+  type        = string
   default     = "2"
   description = "Number of DB instances to create in the cluster"
 }
 
 variable "snapshot_identifier" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Specifies whether or not to create this cluster from a snapshot"
 }
 
 variable "db_name" {
-  type        = "string"
+  type        = string
   description = "Database name"
 }
 
 variable "db_port" {
-  type        = "string"
+  type        = string
   default     = "3306"
   description = "Database port"
 }
 
 variable "admin_user" {
-  type        = "string"
+  type        = string
   default     = "admin"
   description = "(Required unless a snapshot_identifier is provided) Username for the master DB user"
 }
 
 variable "admin_password" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "(Required unless a snapshot_identifier is provided) Password for the master DB user"
 }
 
 variable "retention_period" {
-  type        = "string"
+  type        = string
   default     = "5"
   description = "Number of days to retain backups for"
 }
 
 variable "backup_window" {
-  type        = "string"
+  type        = string
   default     = "07:00-09:00"
   description = "Daily time range during which the backups happen"
 }
 
 variable "maintenance_window" {
-  type        = "string"
+  type        = string
   default     = "wed:03:00-wed:04:00"
   description = "Weekly time range during which system maintenance can occur, in UTC"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `name`, `namespace`, `stage` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "cluster_parameters" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of DB parameters to apply"
 }
 
 variable "instance_parameters" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of DB instance parameters to apply"
 }
 
 variable "cluster_family" {
-  type        = "string"
+  type        = string
   default     = "aurora5.6"
   description = "The family of the DB cluster parameter group"
 }
 
 variable "engine" {
-  type        = "string"
+  type        = string
   default     = "aurora"
   description = "The name of the database engine to be used for this DB cluster. Valid values: `aurora`, `aurora-mysql`, `aurora-postgresql`"
 }
 
 variable "engine_mode" {
-  type        = "string"
+  type        = string
   default     = "provisioned"
   description = "The database engine mode. Valid values: `parallelquery`, `provisioned`, `serverless`"
 }
 
 variable "engine_version" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The version number of the database engine to use"
 }
 
 variable "scaling_configuration" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of nested attributes with scaling properties. Only valid when engine_mode is set to `serverless`"
 }
 
 variable "allowed_cidr_blocks" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of CIDR blocks allowed to access"
 }
@@ -186,7 +186,7 @@ variable "skip_final_snapshot" {
 }
 
 variable "deletion_protection" {
-  type        = "string"
+  type        = string
   description = "If the DB instance should have deletion protection enabled."
   default     = "false"
 }
@@ -207,49 +207,49 @@ variable "rds_monitoring_interval" {
 }
 
 variable "rds_monitoring_role_arn" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The ARN for the IAM role that can send monitoring metrics to CloudWatch Logs"
 }
 
 variable "replication_source_identifier" {
-  type        = "string"
+  type        = string
   description = "ARN of a source DB cluster or DB instance if this DB cluster is to be created as a Read Replica"
   default     = ""
 }
 
 variable "enabled_cloudwatch_logs_exports" {
-  type        = "list"
+  type        = list(string)
   description = "List of log types to export to cloudwatch. The following log types are supported: audit, error, general, slowquery."
   default     = []
 }
 
 variable "performance_insights_enabled" {
-  type        = "string"
+  type        = string
   default     = "false"
   description = "Whether to enable Performance Insights"
 }
 
 variable "performance_insights_kms_key_id" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The ARN for the KMS key to encrypt Performance Insights data. When specifying `performance_insights_kms_key_id`, `performance_insights_enabled` needs to be set to true"
 }
 
 variable "autoscaling_enabled" {
-  type        = "string"
+  type        = string
   default     = "false"
   description = "Whether to enable cluster autoscaling"
 }
 
 variable "autoscaling_policy_type" {
-  type        = "string"
+  type        = string
   default     = "TargetTrackingScaling"
   description = "Autoscaling policy type. `TargetTrackingScaling` and `StepScaling` are supported"
 }
 
 variable "autoscaling_target_metrics" {
-  type        = "string"
+  type        = string
   default     = "RDSReaderAverageCPUUtilization"
   description = "The metrics type to use. If this value isn't provided the default is CPU utilization"
 }
@@ -285,13 +285,14 @@ variable "instance_availability_zone" {
 }
 
 variable "cluster_dns_name" {
-  type        = "string"
+  type        = string
   description = "Name of the cluster CNAME record to create in the parent DNS zone specified by `zone_id`. If left empty, the name will be auto-asigned using the format `master.var.name`"
   default     = ""
 }
 
 variable "reader_dns_name" {
-  type        = "string"
+  type        = string
   description = "Name of the reader endpoint CNAME record to create in the parent DNS zone specified by `zone_id`. If left empty, the name will be auto-asigned using the format `replicas.var.name`"
   default     = ""
 }
+
