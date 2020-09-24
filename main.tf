06ff268e1bf28cbbcac68259830ec3c1f709f0ce
aknysh <andriy.knysh@gmail.com>
2020-09-24 15:42:37 -0400

    Branch for striping inline Security Group rules

--- main.tf
+++ main.tf
@@ -9,40 +9,43 @@ resource "aws_security_group" "default" {
   description = "Allow inbound traffic from Security Groups and CIDRs"
   vpc_id      = var.vpc_id
   tags        = module.this.tags
+
+  ingress = []
+  egress  = []
 }
 
-resource "aws_security_group_rule" "ingress_security_groups" {
-  count                    = module.this.enabled ? length(var.security_groups) : 0
-  description              = "Allow inbound traffic from existing security groups"
-  type                     = "ingress"
-  from_port                = var.db_port
-  to_port                  = var.db_port
-  protocol                 = "tcp"
-  source_security_group_id = var.security_groups[count.index]
-  security_group_id        = join("", aws_security_group.default.*.id)
-}
+#resource "aws_security_group_rule" "ingress_security_groups" {
+#  count                    = module.this.enabled ? length(var.security_groups) : 0
+#  description              = "Allow inbound traffic from existing security groups"
+#  type                     = "ingress"
+#  from_port                = var.db_port
+#  to_port                  = var.db_port
+#  protocol                 = "tcp"
+#  source_security_group_id = var.security_groups[count.index]
+#  security_group_id        = join("", aws_security_group.default.*.id)
+#}
 
-resource "aws_security_group_rule" "ingress_cidr_blocks" {
-  count             = module.this.enabled && length(var.allowed_cidr_blocks) > 0 ? 1 : 0
-  description       = "Allow inbound traffic from existing CIDR blocks"
-  type              = "ingress"
-  from_port         = var.db_port
-  to_port           = var.db_port
-  protocol          = "tcp"
-  cidr_blocks       = var.allowed_cidr_blocks
-  security_group_id = join("", aws_security_group.default.*.id)
-}
+#resource "aws_security_group_rule" "ingress_cidr_blocks" {
+#  count             = module.this.enabled && length(var.allowed_cidr_blocks) > 0 ? 1 : 0
+#  description       = "Allow inbound traffic from existing CIDR blocks"
+#  type              = "ingress"
+#  from_port         = var.db_port
+#  to_port           = var.db_port
+#  protocol          = "tcp"
+#  cidr_blocks       = var.allowed_cidr_blocks
+#  security_group_id = join("", aws_security_group.default.*.id)
+#}
 
-resource "aws_security_group_rule" "egress" {
-  count             = module.this.enabled ? 1 : 0
-  description       = "Allow outbound traffic"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = join("", aws_security_group.default.*.id)
-}
+#resource "aws_security_group_rule" "egress" {
+#  count             = module.this.enabled ? 1 : 0
+#  description       = "Allow outbound traffic"
+#  type              = "egress"
+#  from_port         = 0
+#  to_port           = 0
+#  protocol          = "-1"
+#  cidr_blocks       = ["0.0.0.0/0"]
+#  security_group_id = join("", aws_security_group.default.*.id)
+#}
 
 resource "aws_rds_cluster" "primary" {
   count                               = module.this.enabled && local.is_primary_cluster == true ? 1 : 0
