62cb623b973bd77c9fc006f71d501ef63ef50bd4
Andy Hibbert <anhibber@cisco.com>
2020-10-23 13:06:06 +0100

    global_fixes: Update variables description
    Change-Id: I4326808baf9b724d8ebdc71e675ea5b5b93e451b

--- variables.tf
+++ variables.tf
@@ -59,13 +59,13 @@ variable "db_port" {
 variable "admin_user" {
   type        = string
   default     = ""
-  description = "(Required unless a snapshot_identifier is provided) Username for the master DB user"
+  description = "(Required unless a snapshot_identifier or replication_source_identifier is provided or unless a global_cluster_identifier is provided when the cluster is the secondary cluster of a global database) Username for the master DB user"
 }
 
 variable "admin_password" {
   type        = string
   default     = ""
-  description = "(Required unless a snapshot_identifier is provided) Password for the master DB user"
+  description = "(Required unless a snapshot_identifier or replication_source_identifier is provided or unless a global_cluster_identifier is provided when the cluster is the secondary cluster of a global database) Password for the master DB user"
 }
 
 variable "retention_period" {
