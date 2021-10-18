bc1d8b22d119bfb907d0c0161fffe2d13059fbd9
nitrocode <nitrocode@users.noreply.github.com>
2021-10-18 14:19:22 -0500

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -5,62 +5,73 @@ locals {
   performance_insights_kms_key_id     = local.performance_insights_create_kms_key ? module.performance_insights_kms_key[1].key_arn : var.performance_insights_kms_key_id
 
   # This policy is copied from aws/rds
-  kms_policy = <<-EOT
-  {
-      "Version": "2012-10-17",
-      "Id": "auto-rds-2",
-      "Statement": [
-          {
-              "Sid": "Allow management of the key",
-              "Effect": "Allow",
-              "Principal": {
-                  "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-              },
-              "Action": "kms:*",
-              "Resource": "*"
-          },
-          {
-              "Sid": "Allow access through RDS for all principals in the account that are authorized to use RDS",
-              "Effect": "Allow",
-              "Principal": {
-                  "AWS": "*"
-              },
-              "Action": [
-                  "kms:Encrypt",
-                  "kms:Decrypt",
-                  "kms:ReEncrypt*",
-                  "kms:GenerateDataKey*",
-                  "kms:CreateGrant",
-                  "kms:ListGrants",
-                  "kms:DescribeKey"
-              ],
-              "Resource": "*",
-              "Condition": {
-                  "StringEquals": {
-                      "kms:ViaService": "rds.${data.aws_region.current.name}.${data.aws_partition.current.dns_suffix}",
-                      "kms:CallerAccount": "${data.aws_caller_identity.current.account_id}"
-                  }
-              }
-          },
-          {
-              "Sid": "Allow direct access to key metadata to the account",
-              "Effect": "Allow",
-              "Principal": {
-                  "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-              },
-              "Action": [
-                  "kms:Describe*",
-                  "kms:Get*",
-                  "kms:List*",
-                  "kms:RevokeGrant"
-              ],
-              "Resource": "*"
-          }
-      ]
-  }
-  EOT
+  kms_policy = data.aws_iam_policy_document.performance_insights.json
 }
 
+data "aws_iam_policy_document" "performance_insights" {
+  statement {
+    sid       = "Allow management of the key"
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["kms:*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    sid       = "Allow access through RDS for all principals in the account that are authorized to use RDS"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:CreateGrant",
+      "kms:ListGrants",
+      "kms:DescribeKey",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = ["rds.${data.aws_region.current.name}.${data.aws_partition.current.dns_suffix}"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+
+  statement {
+    sid       = "Allow direct access to key metadata to the account"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "kms:Describe*",
+      "kms:Get*",
+      "kms:List*",
+      "kms:RevokeGrant",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+}
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
