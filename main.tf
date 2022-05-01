5eaaa83a90695ea5f5e2314ffa2ae4d279381871
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2022-05-01 01:21:58 +0000

    Auto Format

--- main.tf
+++ main.tf
@@ -118,8 +118,8 @@ resource "aws_rds_cluster" "primary" {
   dynamic "serverlessv2_scaling_configuration" {
     for_each = var.serverlessv2_scaling_configuration[*]
     content {
-      max_capacity             = serverlessv2_scaling_configuration.value.max_capacity
-      min_capacity             = serverlessv2_scaling_configuration.value.min_capacity
+      max_capacity = serverlessv2_scaling_configuration.value.max_capacity
+      min_capacity = serverlessv2_scaling_configuration.value.min_capacity
     }
   }
 
