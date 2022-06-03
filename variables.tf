2eaa418077b7cfc5889b5eb47c184ffa440915d5
matharoo <matharoo89@gmail.com>
2022-06-03 17:30:13 -0600

    setting storage_type var to io1 as defined by terraform docs

--- variables.tf
+++ variables.tf
@@ -207,7 +207,7 @@ variable "storage_encrypted" {
 variable "storage_type" {
   type        = string
   description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), or 'io1' (provisioned IOPS SSD)"
-  default     = "standard"
+  default     = "io1"
 }
 
 variable "iops" {
