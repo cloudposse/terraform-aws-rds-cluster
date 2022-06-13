2fed1b2847a1fe88c15214a8b99fb4b8446babb1
GongYi <topikachu@163.com>
2022-06-13 13:47:50 +0800

    Add aurora serverlessv2 support (#138)

--- examples/serverlessv2_postgres/outputs.tf
+++ examples/serverlessv2_postgres/outputs.tf
@@ -1,51 +1,51 @@
 output "database_name" {
-  value       = module.rds_cluster.database_name
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.database_name
   description = "Database name"
 }
 
 output "master_username" {
-  value       = module.rds_cluster.master_username
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.master_username
   description = "Username for the master DB user"
   sensitive   = true
 }
 
 output "cluster_identifier" {
-  value       = module.rds_cluster.cluster_identifier
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.cluster_identifier
   description = "Cluster Identifier"
 }
 
 output "arn" {
-  value       = module.rds_cluster.arn
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.arn
   description = "Amazon Resource Name (ARN) of the cluster"
 }
 
 output "endpoint" {
-  value       = module.rds_cluster.endpoint
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.endpoint
   description = "The DNS address of the RDS instance"
 }
 
 output "reader_endpoint" {
-  value       = module.rds_cluster.reader_endpoint
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.reader_endpoint
   description = "A read-only endpoint for the Aurora cluster, automatically load-balanced across replicas"
 }
 
 output "master_host" {
-  value       = module.rds_cluster.master_host
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.master_host
   description = "DB Master hostname"
 }
 
 output "replicas_host" {
-  value       = module.rds_cluster.replicas_host
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.replicas_host
   description = "Replicas hostname"
 }
 
 output "dbi_resource_ids" {
-  value       = module.rds_cluster.dbi_resource_ids
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.dbi_resource_ids
   description = "List of the region-unique, immutable identifiers for the DB instances in the cluster"
 }
 
 output "cluster_resource_id" {
-  value       = module.rds_cluster.cluster_resource_id
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.cluster_resource_id
   description = "The region-unique, immutable identifie of the cluster"
 }
 
@@ -65,16 +65,16 @@ output "vpc_cidr" {
 }
 
 output "security_group_id" {
-  value       = module.rds_cluster.security_group_id
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.security_group_id
   description = "Security Group ID"
 }
 
 output "security_group_arn" {
-  value       = module.rds_cluster.security_group_arn
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.security_group_arn
   description = "Security Group ARN"
 }
 
 output "security_group_name" {
-  value       = module.rds_cluster.security_group_name
+  value       = module.rds_cluster_aurora_serverlessv2_postgres_13.security_group_name
   description = "Security Group name"
 }
---
+++ test/src/examples_serverlessv2_postgres_test.go
@@ -0,0 +1,64 @@
+package test
+
+import (
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test the Terraform module in examples/complete using Terratest.
+func TestExamplesServerlessv2Postgres(t *testing.T) {
+	t.Parallel()
+
+	rand.Seed(time.Now().UnixNano())
+
+	randId := strconv.Itoa(rand.Intn(100000))
+	attributes := []string{randId}
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../../examples/serverlessv2_postgres",
+		Upgrade:      true,
+		// Variables to pass to our Terraform code using -var-file options
+		VarFiles: []string{"fixtures.us-east-2.tfvars"},
+		Vars: map[string]interface{}{
+			"attributes": attributes,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the value of an output variable
+	vpcCidr := terraform.Output(t, terraformOptions, "vpc_cidr")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, "172.16.0.0/16", vpcCidr)
+
+	// Run `terraform output` to get the value of an output variable
+	privateSubnetCidrs := terraform.OutputList(t, terraformOptions, "private_subnet_cidrs")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, []string{"172.16.0.0/19", "172.16.32.0/19"}, privateSubnetCidrs)
+
+	// Run `terraform output` to get the value of an output variable
+	publicSubnetCidrs := terraform.OutputList(t, terraformOptions, "public_subnet_cidrs")
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, []string{"172.16.96.0/19", "172.16.128.0/19"}, publicSubnetCidrs)
+
+	// Run `terraform output` to get the value of an output variable
+	clusterIdentifier := terraform.Output(t, terraformOptions, "cluster_identifier")
+	expectedClusterIdentifier := "eg-test-rds-cluster-" + randId
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, expectedClusterIdentifier, clusterIdentifier)
+
+	// Run `terraform output` to get the value of an output variable
+	arn := terraform.Output(t, terraformOptions, "arn")
+	// Verify we're getting back the outputs we expect
+	assert.Contains(t, arn, ":cluster:eg-test-rds-cluster")
+}
