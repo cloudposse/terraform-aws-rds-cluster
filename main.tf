0e4c32f1d4b4538645328e145843a4092df2696c
matharoo <matharoo89@gmail.com>
2022-07-11 14:46:40 -0700

    in case of multi a-z rds cluster, skip creating aws_rds_cluster_instance resource when engine is non-aurora

--- main.tf
+++ main.tf
@@ -238,7 +238,7 @@ resource "aws_rds_cluster" "secondary" {
 }
 
 resource "aws_rds_cluster_instance" "default" {
-  count                                 = local.cluster_instance_count
+  count                                 = contains(regex("^(?:.*(aurora))?.*$",var.engine),"aurora") ? local.cluster_instance_count : 0
   identifier                            = var.cluster_identifier == "" ? "${module.this.id}-${count.index + 1}" : "${var.cluster_identifier}-${count.index + 1}"
   cluster_identifier                    = coalesce(join("", aws_rds_cluster.primary.*.id), join("", aws_rds_cluster.secondary.*.id))
   instance_class                        = var.serverlessv2_scaling_configuration != null ? "db.serverless" : var.instance_type
