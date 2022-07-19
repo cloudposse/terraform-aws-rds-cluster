c84cb851a4cc9d4a2d6bc8372f476ea6eb06513e
matharoo <matharoo89@gmail.com>
2022-07-19 12:52:49 -0700

    updating aws provider to 4.23.0 that fixes the rebooting state failure when creating multi a-z cluster

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.17.0"
+      version = ">= 4.23.0"
     }
     null = {
       source  = "hashicorp/null"
