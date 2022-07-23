12428083b1ba059923ae08e904e2fbb6a65190ed
Adi Davidovich <adi.davidovich@gong.io>
2022-07-23 10:22:11 +0300

    fix: pass value to db_instance_parameter_group_name parameter when upgrading major version

--- main.tf
+++ main.tf
@@ -97,6 +97,7 @@ resource "aws_rds_cluster" "primary" {
   engine                              = var.engine
   engine_version                      = var.engine_version
   allow_major_version_upgrade         = var.allow_major_version_upgrade
+  db_instance_parameter_group_name    = var.allow_major_version_upgrade ? join("", aws_db_parameter_group.default.*.name) : null
   engine_mode                         = var.engine_mode
   iam_roles                           = var.iam_roles
   backtrack_window                    = var.backtrack_window
