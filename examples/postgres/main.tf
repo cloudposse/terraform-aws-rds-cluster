96e72e021ba4f1f4289629c483f97ac24e1e7edc
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2022-08-15 21:28:54 +0000

    Auto Format

--- examples/postgres/main.tf
+++ examples/postgres/main.tf
@@ -28,24 +28,24 @@ module "subnets" {
 module "rds_cluster" {
   source = "../../"
 
-  engine              = var.engine
-  engine_mode         = var.engine_mode
-  engine_version      = var.engine_version
-  cluster_family      = var.cluster_family
-  cluster_size        = var.cluster_size
-  admin_user          = var.admin_user
-  admin_password      = var.admin_password
-  db_name             = var.db_name
-  instance_type       = var.instance_type
+  engine                    = var.engine
+  engine_mode               = var.engine_mode
+  engine_version            = var.engine_version
+  cluster_family            = var.cluster_family
+  cluster_size              = var.cluster_size
+  admin_user                = var.admin_user
+  admin_password            = var.admin_password
+  db_name                   = var.db_name
+  instance_type             = var.instance_type
   db_cluster_instance_class = var.db_cluster_instance_class
-  vpc_id              = module.vpc.vpc_id
-  subnets             = module.subnets.private_subnet_ids
-  security_groups     = [module.vpc.vpc_default_security_group_id]
-  deletion_protection = var.deletion_protection
-  autoscaling_enabled = var.autoscaling_enabled
-  storage_type        = var.storage_type
-  iops                = var.iops
-  allocated_storage   = var.allocated_storage
+  vpc_id                    = module.vpc.vpc_id
+  subnets                   = module.subnets.private_subnet_ids
+  security_groups           = [module.vpc.vpc_default_security_group_id]
+  deletion_protection       = var.deletion_protection
+  autoscaling_enabled       = var.autoscaling_enabled
+  storage_type              = var.storage_type
+  iops                      = var.iops
+  allocated_storage         = var.allocated_storage
 
   context = module.this.context
 }
