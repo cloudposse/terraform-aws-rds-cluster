a175086d114a847f4f7b8a3f87ca7b2f469ba174
matharoo <matharoo89@gmail.com>
2022-08-15 14:27:45 -0700

    test for postgres cluster setup

---
+++ examples/postgres/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.17.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+  }
+}
