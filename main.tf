9d5db99714fb6a8ecfdc062fe0474f2e09f03487
Julien Francoz <julien@les-tilleuls.coop>
2025-02-04 10:49:16 +0100

    add reader_independent_scaling input
    allow to scale reader independently from primary using promotion_tier >= 2
    
    https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/aurora-serverless-v2.how-it-works.html#aurora-serverless-v2.how-it-works.scaling

--- main.tf
+++ main.tf
@@ -368,6 +368,7 @@ resource "aws_rds_cluster_instance" "default" {
   preferred_maintenance_window          = var.maintenance_window
   copy_tags_to_snapshot                 = var.copy_tags_to_snapshot
   ca_cert_identifier                    = var.ca_cert_identifier
+  promotion_tier                        = var.reader_independent_scaling ? 2 : 0
 
   dynamic "timeouts" {
     for_each = var.timeouts_configuration
