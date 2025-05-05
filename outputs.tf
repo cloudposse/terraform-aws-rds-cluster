61415b5a4ef28d37d221d818d4e7092db73afa07
Dome <domenic@gerhold.email>
2025-05-05 09:00:03 +0000

    Rebase

--- outputs.tf
+++ outputs.tf
@@ -34,9 +34,10 @@ output "port" {
   description = "DB port"
 }
 
+# See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster.html#master_user_secret
 output "admin_user_secret" {
-  value       = local.is_regional_cluster ? join("", aws_rds_cluster.primary[*].master_user_secret) : join("", aws_rds_cluster.secondary[*].master_user_secret)
-  description = "The secret manager attributes (only when `manage_admin_user_password` is `true` ) See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster.html#master_user_secret"
+  value       = var.manage_admin_user_password ? join("", aws_rds_cluster.primary[*].master_user_secret) : []
+  description = "The secret manager attributes for the managed admin user password (`master_user_secret`)."
 }
 
 output "master_host" {
