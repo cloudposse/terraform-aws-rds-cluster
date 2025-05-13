4e116cf3843f9d351d4e79d5488dfca1911464c0
Dome <domenic@gerhold.email>
2025-05-13 15:51:08 +0200

    Update outputs.tf
    Follow Review suggestion
    
    Co-authored-by: Joe Niland <joe@originalmind.com.au>

--- outputs.tf
+++ outputs.tf
@@ -31,7 +31,7 @@ output "reader_endpoint" {
 
 # See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster.html#master_user_secret
 output "admin_user_secret" {
-  value       = var.manage_admin_user_password ? join("", aws_rds_cluster.primary[*].master_user_secret) : []
+  value       = var.manage_admin_user_password ? concat(aws_rds_cluster.primary[*].master_user_secret) : []
   description = "The secret manager attributes for the managed admin user password (`master_user_secret`)."
 }
 
