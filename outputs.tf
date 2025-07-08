7acb02db6bc7c997e6ad7cbf9798b9e27b23a764
Domenic G. <domenic@gerhold.email>
2025-07-08 06:48:39 +0200

    Add output variable for `master_user_secret` (#260)
    Co-authored-by: Joe Niland <joe@originalmind.com.au>

--- outputs.tf
+++ outputs.tf
@@ -34,6 +34,12 @@ output "port" {
   description = "DB port"
 }
 
+# See: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster.html#master_user_secret
+output "admin_user_secret" {
+  value       = var.manage_admin_user_password ? concat(aws_rds_cluster.primary[*].master_user_secret) : []
+  description = "The secret manager attributes for the managed admin user password (`master_user_secret`)."
+}
+
 output "master_host" {
   value       = module.dns_master.hostname
   description = "DB Master hostname"
