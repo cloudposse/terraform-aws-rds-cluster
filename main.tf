0ffa737e5335f3ba0e4da51fa1df5bdbd4faea6c
yangci <yangciouou@gmail.com>
2025-10-29 13:04:03 -0400

    feat: support setting var.database_insights_mode (#274)
    * feat: support setting var.database_insights_mode
    
    * Apply suggestion from @oycyc

--- main.tf
+++ main.tf
@@ -166,6 +166,7 @@ resource "aws_rds_cluster" "primary" {
   performance_insights_enabled          = var.performance_insights_enabled
   performance_insights_kms_key_id       = var.performance_insights_kms_key_id
   performance_insights_retention_period = var.performance_insights_retention_period
+  database_insights_mode                = var.database_insights_mode
 
   depends_on = [
     aws_db_subnet_group.default,
@@ -269,6 +270,7 @@ resource "aws_rds_cluster" "secondary" {
   port                                = var.db_port
   enable_global_write_forwarding      = var.enable_global_write_forwarding
   enable_local_write_forwarding       = var.enable_local_write_forwarding
+  database_insights_mode              = var.database_insights_mode
 
   depends_on = [
     aws_db_subnet_group.default,
