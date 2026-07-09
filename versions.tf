56c6c266deaeedc6092d3dc0804c918ce6cf67f8
gokul-das-ext <gdas-ext@drivewealth.com>
2026-07-09 18:06:12 +0530

    reverted the terraform provider version changes

--- versions.tf
+++ versions.tf
@@ -1,5 +1,6 @@
 terraform {
-  required_version = ">= 1.3.0"
+  # >= 1.5.0 required for `check` blocks (used to emit deprecation warnings)
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = {
